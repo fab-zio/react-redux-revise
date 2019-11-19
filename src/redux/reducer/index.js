1cad33c83420aa1ad4d046353c2b8b94ebd58e55
fab-zio <fabrizio.musa@yahoo.com>
2019-11-19 23:50:04 +0100

    reducer set

---
+++ src/redux/reducer/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import combineReducers from './courseReducer';
+
+const rootReducer = combineReducers({
+    course: course
+});
+
+export default rootReducer;
