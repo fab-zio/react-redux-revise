51009221378956324fe098d0b6b73db240208ef1
fab-zio <fabrizio.musa@yahoo.com>
2020-04-27 01:11:00 +0200

    first hooks component

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react'; //second class is hooks to handle state and side effect in function components
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/action/courseActions';
 import * as authorActions from '../../redux/action/authorActions';
-import { bindActionCreators } from 'redux'; //if u wanna remove this binder follow uncomment code 
+// import { bindActionCreators } from 'redux'; //if u wanna remove this binder follow uncomment code 
 import PropTypes from 'prop-types';
 
 
 
 
-class ManageCoursePage extends React.Component {
+function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
 
-    componentDidMount() {
+    useEffect(() => {
 
         // const { courses, authors, loadCourses, loadAuthors } = this.props;
         // if (courses.length === 0) {
@@ -24,37 +24,35 @@ class ManageCoursePage extends React.Component {
         //         alert('Loading author failed due' + error)
         //     })
         // }
-        const { courses, actions, authors } = this.props;
 
-        // if (courses.length === 0) {
-        actions.loadCourses().catch(error => {
-            alert('Loading courses failed due' + error)
-        })
-        // }
+        if (courses.length === 0) {
+            loadCourses().catch(error => {
+                alert('Loading courses failed due' + error)
+            })
+        }
 
-        // if (authors.length === 0) {
-        actions.loadAuthors().catch(error => {
-            alert('Loading author failed due' + error)
-        })
-        // }
-    }
+        if (authors.length === 0) {
+            loadAuthors().catch(error => {
+                alert('Loading author failed due' + error)
+            })
+        }
+    })
 
 
-    render() {
-        return (
-            <>
-                <h1>Manage Course</h1>
-            </>
-        )
-    }
+    return (
+        <>
+            <h1>Manage Course</h1>
+        </>
+    )
+
 }
 
 ManageCoursePage.propTypes = {
     authors: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired,
+    // actions: PropTypes.object.isRequired,
     courses: PropTypes.array.isRequired,
-    // loadCourses: PropTypes.func.isRequired,
-    // loadAuthors: PropTypes.func.isRequired,
+    loadCourses: PropTypes.func.isRequired,
+    loadAuthors: PropTypes.func.isRequired,
 }
 
 
@@ -67,20 +65,20 @@ function mapStateToProps(state) {
 
 
 // // OBJECT WAY
-// const mapDispatchToProps = {
-//     loadCourses: courseActions.loadCourses,
-//     loadAuthors: authorActions.loadAuthors,
-// };
+const mapDispatchToProps = {
+    loadCourses: courseActions.loadCourses,
+    loadAuthors: authorActions.loadAuthors,
+};
 
 
-function mapDispatchToProps(dispatch) {
-    return {
-        actions: {
-            loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-            loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
-        }
-    }
-}
+// function mapDispatchToProps(dispatch) {
+//     return {
+//         actions: {
+//             loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+//             loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+//         }
+//     }
+// }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
 
